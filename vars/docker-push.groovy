1723dc27d5a61f362dd7c6a60a834edf5a16ba68
Lincy Varghese <lincytv@gmail.com>
2021-06-26 01:50:33 +0530

    adding username

--- vars/docker-push.groovy
+++ vars/docker-push.groovy
@@ -1,6 +1,4 @@
-def call(repo,image_name,env)
-withCredentials([usernamePassword(credentialsId: 'docker_login_dev', usernameVariable: 'Username', passwordVariable: 'Password')])
-stage 'test' {
+def call(repo,image_name,env) {
     sh """
         echo "docker push"
         docker login ${repo} -u ${Username} -p ${Password}
