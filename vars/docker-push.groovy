64ec2235e12cb90305d5cbc298d9d187453df209
Lincy Varghese <lincytv@gmail.com>
2021-06-25 20:20:45 +0530

    docker pushaa

--- vars/docker-push.groovy
+++ vars/docker-push.groovy
@@ -3,6 +3,7 @@ withCredentials([credentialid: "docker_login_${env}", username: $username, passw
 {
     sh"""
         docker login ${repo} -u ${username} -p ${password}
-        docker push ${image_name} ${repo}   
+        docker tag ${image_name} ${repo}/${image_name}
+        docker push ${repo}/${image_name}    
      """
 }
